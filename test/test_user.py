d825f3bac3eff1314669408b89f8fe58a1eb9f0f
Allen Zhong <zhong.allen@gmail.com>
2017-08-04 23:23:05 +1200

    create user test

---
+++ test/test_user.py
@@ -0,0 +1,17 @@
+import unittest
+# from processor.user import User
+
+class TestUser(unittest.TestCase):
+
+  def test_create_user(self):
+    NotImplementedError
+
+  def test_capitalize_name(self):
+    NotImplementedError
+
+  def test_downcase_email(self):
+    NotImplementedError
+
+  def test_verify_email(self):
+    NotImplementedError
+
