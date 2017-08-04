615efcf0c109b61eeb7534d2bba46ab4ae73d21d
Allen Zhong <zhong.allen@gmail.com>
2017-08-04 23:31:23 +1200

    add data_processor test stub

---
+++ test/test_data_processor.py
@@ -0,0 +1,15 @@
+import unittest
+# from processor.data_processor import DataProcessor
+
+class TestDataProcessor(unittest.TestCase):
+
+  def test_read_data_from_csv_file_with_filename(self):
+    NotImplementedError
+
+  def test_create_table_in_specified_db_with_user_and_pwd(self):
+    NotImplementedError
+
+  def test_insert_data_into_table_existing(self):
+    NotImplementedError
+
+
