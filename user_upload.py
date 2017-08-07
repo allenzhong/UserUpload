302387bf0db1f3cf1cdf887d65909bc0f19bff79
Allen Zhong <zhong.allen@gmail.com>
2017-08-08 10:24:42 +1200

    adjust dry run parameter’s bug

--- user_upload.py
+++ user_upload.py
@@ -8,11 +8,12 @@ def parse_arguments():
   parser.add_argument('-?', '--help', action='help', help='Output the above list of directives with details.',)
   parser.add_argument('-f', '--file', help='Name of the CSV to be parsed', )
   parser.add_argument('-c', '--create_table', action='store_true', help='this will cause the MySQL users table to be built (and no further action will be taken)')
-  parser.add_argument('-r', '--dry_run', help='this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won\'t be altered.')
+  parser.add_argument('-r', '--dry_run', action='count', help='this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won\'t be altered.')
   parser.add_argument('-u', '--username', required=True, help='MySQL username')
   parser.add_argument('-p', '--password', required=True, help='MySQL password')
   parser.add_argument('-d', '--database', required=True, help='MySQL Database that will create table and insert data')
   parser.add_argument('-h', '--host', default='localhost', help='MySQL host')
+
   args = parser.parse_args()
   handle_process(args)
 
@@ -37,7 +38,7 @@ def handle_process(args):
   elif filename is not None:
     processor = DataProcessor(filename)
     users = processor.fetch_data()
-    processor.insert_data(users, table_name="users", host=host, username=username, password=password, database=database, dry_run=(args.dry_run is not None))
+    processor.insert_data(users, table_name="users", host=host, username=username, password=password, database=database, dry_run=(args.dry_run > 0))
 
 if __name__=="__main__":
   try:
